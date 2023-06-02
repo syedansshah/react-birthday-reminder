b00599fa5b1f591b15579a81310b3edfc022e63c
ans <syed.shah@safarifoneict.com>
2023-06-02 16:08:55 +0500

    initial birthday reminder portal

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import data from './data';
+import List from './List';
+function App() {
+  return (
+    <h2>Birthday Reminder Portal</h2>
+    );
+}
+
+export default App;
