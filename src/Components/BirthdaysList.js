f531c567ec9ebd82e91103cbc1060ad92672a259
ans <syed.shah@safarifoneict.com>
2023-06-05 15:14:16 +0500

    fixed typo mistakes

--- src/Components/BirthdaysList.js
+++ src/Components/BirthdaysList.js
@@ -3,17 +3,18 @@ import People from '../Data/people';
 import List from './List';
 import Button from '@mui/material/Button';
 
+
 function BirhtdayList() {
     const [people,setPeople] = useState(People)
     return (
-      <main>
+      <>
         <section className='container'>
-          <h3>{people.length} Birhtday today </h3>
+          <h3>{people.length} Birthday today </h3>
           <List people={people}></List>
           <Button style={{"backgroundColor":"#ff8b00"} } variant="contained" onClick={()=>setPeople([])}>Clear all</Button>
           <Button variant="contained" onClick={()=>setPeople(People)}>Reload</Button>
         </section>
-      </main>
+      </>
   
     );
   }
