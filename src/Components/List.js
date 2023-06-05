a67d53ea3855b6e1a3c3a54437293d7bc5206372
ans <syed.shah@safarifoneict.com>
2023-06-05 14:51:46 +0500

    - updated list

--- src/Components/List.js
+++ src/Components/List.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { useState } from 'react';
+import moment from 'moment/moment';
 
 const List = ({people}) => {
- 
+  
   return (
     <>
       <h2>list component</h2>
       {people.map((person)=>{
         const {id, name, age, image, dob} = person;
-        let profilePic = process.env.PUBLIC_URL + 'userImg.JPG';
-         
+        
+        let profilePic = process.env.IMG_URL + 'userImg.JPG';
+
         if(image){
           profilePic= image;
         }
@@ -18,7 +20,7 @@ const List = ({people}) => {
             <img  src={profilePic} alt={name}></img>
             <div>
               <h4>{name}</h4>
-              <p>{dob} - {age} years</p>
+              <p>{dob}</p>
             </div>
           </article>
         ) 
